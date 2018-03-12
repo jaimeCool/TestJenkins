0b4abd173c7152c462da0e87ba283492c51db84a
Yaso <jie.yang12@ele.me>
2018-03-12 14:07:51 +0800

    Initial Commit

---
+++ TestJenkins/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestJenkins
+//
+//  Created by Yaso on 12/03/2018.
+//  Copyright © 2018 Yaso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestJenkins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestJenkins
+//
+//  Created by Yaso on 12/03/2018.
+//  Copyright © 2018 Yaso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
